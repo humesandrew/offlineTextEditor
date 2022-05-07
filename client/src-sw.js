9c4f1fa12487b81a99730105422731227a334c0a
Andy Humes <andrew.humes@du.edu>
2022-05-07 11:57:30 -0600

    workingon install

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+
+  // this is the function that will filter the requests we want to cache. Here that means JS and CSS//
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
